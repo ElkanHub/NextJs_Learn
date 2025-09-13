a6e29562294e5cdc0d252ae9a31f74803279c6de
Elkanah Donkor <elkanahdonkor@gmail.com>
2025-09-13 13:53:57 +0000

    finished the course.... one outsrtanding error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import { Metadata } from 'next';
 import '@/app/ui/global.css';
 
 import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -12,3 +14,12 @@ export default function RootLayout({
     </html>
   );
 }
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
